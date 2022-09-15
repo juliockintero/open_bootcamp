c9ae010c1abba6a240a2b4f7648affec54f7be3c
Julio Cesar Quintero Leal <75279105+juliockintero@users.noreply.github.com>
2022-09-15 15:40:22 -0500

    PARTE 1 DEL PRIMER EJERCICIO

---
+++ Ejercicio1/Ejercicio1_parte1.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+    System.out.println(sumar(1,2,3));
+    }
+
+
+public static int sumar(int a, int b, int c) {
+    return a+b+c ;
+    
+  }
+}
