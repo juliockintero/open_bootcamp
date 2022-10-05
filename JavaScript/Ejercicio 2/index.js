95336987ae9a3b336d5d04574385f89d52c0121b
juliockintero <juliockintero@hotmail.com>
2022-10-04 22:30:31 -0500

    Ejercicio 2 Creando una lista en js

---
+++ JavaScript/Ejercicio 2/index.js
@@ -0,0 +1,7 @@
+const lista = ["Julio Cesar Quintero Leal", 24, true, new Date(1998, 0, 25),
+    {
+        titulo: 'Harry potter y la piedra filosofal', autor: 'J.K Rowling', anio: 1999, url: "https://www.amazon.com/-/es/J-K-Rowling/dp/8478884459"
+    }
+]
+
+console.log(typeof lista[3])
