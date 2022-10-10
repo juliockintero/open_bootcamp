ba25a264a8bf6e16d881f6b3931849117364a25b
juliockintero <juliockintero@hotmail.com>
2022-10-09 22:02:19 -0500

    Ejercicio de numeros

---
+++ JavaScript/Ejercicio 5/index.js
@@ -0,0 +1,21 @@
+// Ejercicio de numeros.
+
+let alturaEnCm = 173
+let alturaEnMts = 1.73
+let pesoEnKgs = 60.5
+let alturaRed = alturaEnMts.toFixed()   
+let alturaRed2 = alturaEnMts.toFixed(3)
+console.log( alturaRed)
+console.log( alturaRed2)
+
+
+let minValorjs = Number.MIN_VALUE;
+
+let maxValorjs = Number.MAX_VALUE;
+
+let maxValorjsMasUno = maxValorjs + 1
+console.log(maxValorjs)
+
+let Iguales = maxValorjsMasUno == maxValorjs
+
+console.log(Iguales)
