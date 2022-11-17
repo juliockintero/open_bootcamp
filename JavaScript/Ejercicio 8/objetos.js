9cce380a962c367d21df9d6156913c0c0edc3b54
juliockintero <juliockintero@hotmail.com>
2022-11-17 15:59:45 -0500

    Ejercicio con obejetoss

---
+++ JavaScript/Ejercicio 8/objetos.js
@@ -0,0 +1,38 @@
+
+const datosPersonales = {
+    nombre: 'Julio Cesar',
+    apellido: 'Quintero Leal',
+    edad: 20,
+    altura: 174,
+    eresDesarrollador: true
+}
+
+let edad = datosPersonales.edad
+
+console.log(edad)
+
+const listdatosPersonales = [
+    datosPersonales,
+
+    {
+        nombre: 'Sebastian',
+        telefono: 32149084234,
+        edad: 23,
+        email: 'sebas@gmail.com',
+        ciudad: 'Casanare',
+        direccion: 'Cr 31-734'
+    },
+    {
+        nombre: 'Eduardo',
+        telefono: 3214908768,
+        edad: 30,
+        email: 'edtorres@gmail.com',
+        ciudad: 'Casanare',
+        direccion: 'Cr 31-734'
+    }
+
+]
+
+const ordenedList = listdatosPersonales.sort((a, b) => a.edad - b.edad);
+
+console.log(ordenedList)
