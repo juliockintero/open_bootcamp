7ff209059f5bfcd4775cea5ce67072d4445f45d8
juliockintero <juliockintero@hotmail.com>
2022-11-28 15:54:57 -0500

    Ejercicio con cookies, localdata y session

--- JavaScript/Ejercicio-18/index.js
+++ JavaScript/Ejercicio-18/index.js
@@ -6,8 +6,7 @@ const nombreCompleto = {
     "apellido": apellido
 }
 
-sessionStorage.setItem('nombreCompleto', JSON.stringify(nombreCompleto))
-localStorage.setItem('nombreCompleto', JSON.stringify(nombreCompleto))
+//sessionStorage.setItem('nombreCompleto', JSON.stringify(nombreCompleto))
+//localStorage.setItem('nombreCompleto', JSON.stringify(nombreCompleto))
 
-document.cookie = 'nombreCompleto=' + JSON.stringify(nombreCompleto) +
-    ";expires=Mon, 28 Nov 2022 20:59:59 GMT;"
+//document.cookie = 'nombreCompleto=' + JSON.stringify(nombreCompleto) + ";expires=Mon, 28 Nov 2022 20:59:59 GMT;"
