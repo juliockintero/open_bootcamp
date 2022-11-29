7d1a4d0dcb74f3d8003550c6803177a5d6342346
juliockintero <juliockintero@hotmail.com>
2022-11-28 23:12:57 -0500

    Ejercicio 19- Drag and Drop

---
+++ JavaScript/Ejercicio-19/index.js
@@ -0,0 +1,50 @@
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+const papelera = document.querySelectorAll(".papelera")
+
+console.log(papelera)
+console.log(secciones)
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event => {
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".imagen-fantasma")
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
+    })
+
+    parrafo.addEventListener("dragend", () => {
+        // console.log("He terminado de arrastrar")
+        parrafo.classList.remove("dragging")
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy" // copy por defecto
+        // console.log("Drag Over")
+        //
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        // console.log("Párrafo id: ", id_parrafo)
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
+
+papelera[0].addEventListener("drop", event => {
+    console.log("Drop")
+    const id_parrafo = event.dataTransfer.getData('id')
+    const parrafo = document.getElementById(id_parrafo)
+    parrafo.remove()
+})
+
+papelera[0].addEventListener("dragover", event => {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "copy"
+})
